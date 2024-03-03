60e11df57efa0af67b35179b0c98bfe4fdb1414b
Hiperion23 <wenramirez712@gmail.com>
2024-03-02 19:13:57 -0500

    v2

--- Bot.py
+++ Bot.py
@@ -2,9 +2,11 @@ from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import telebot
 import requests
+import openai
 
 # Crea un objeto bot con tu token
 bot = telebot.TeleBot("6609925188:AAH7HTH5cGTa35GG90PORjpoV7OPIJB87T8")
+openai.api_key = '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'
 
 # Configura las credenciales de IBM Watson Speech to Text
 authenticator = IAMAuthenticator('CnxW9oIaYALaqdjlVLRXfflgIVi1cULZvFr-RM8HGtDj')
@@ -19,19 +21,31 @@ speech_to_text.set_service_url('https://api.au-syd.speech-to-text.watson.cloud.i
 # Maneja el comando '/start'
 @bot.message_handler(commands=['start', 'help'])
 def handle_start(message):
-    bot.reply_to(message, "Hola! Soy un bot de ejemplo.")
+    bot.reply_to(message, "Hola! Soy un bot, Me llamo Sykomv.")
 
 # Maneja mensajes de texto
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     # Respuestas condicionales
-    if message.text.lower() == 'hola' and 'Hola' and 'ola' and 'Ola':
-        bot.reply_to(message, "Hola! ¿Cómo estás?")
-    elif message.text.lower() == 'adios':
-        bot.reply_to(message, "¡Hasta luego!")
+    if message.text.lower() == 'hola' or  message.text.lower() == 'adios':
+        bot.reply_to(message, "Hola! ¿Cómo estás?" if message.text.lower() == 'hola' else "¡Hasta luego!") 
     else:
-        bot.reply_to(message, "Lo siento, no entendí tu mensaje.")
-
+        # Si no es 'hola' ni 'adios', utiliza ChatGPT para generar una respuesta
+        try:
+            response = openai.Completion.create(
+                engine="text-davinci-001",  # Reemplaza "gpt-3.5-turbo" con el modelo adecuado
+                prompt=f"You are a helpful assistant. User: {message.text.lower()}",
+                temperature=0.7,
+                max_tokens=50,
+                n=1,  # Indica la cantidad de respuestas a generar (aquí solo se necesita 1)
+                stop=None,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+            bot.reply_to(message, response.choices[0].text.strip())  # Envía la respuesta de ChatGPT al usuario
+        except Exception as e:
+            print("Error al obtener la respuesta de ChatGPT:", e)
+            bot.reply_to(message, "Lo siento, hubo un problema al procesar tu mensaje.")
 
 # Maneja mensajes de voz
 @bot.message_handler(content_types=['voice'])
